a1c9b6b5c87354607d1e16a71d99516b8499b404
Paul Savignano <Paul.Savignano@gmail.com>
2017-01-09 16:04:22 -0800

    Add bcrypt salt to password.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,34 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide an email and password' });
+  }
+
+  // Check if a user email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+    // If user email exists return error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' })
+    }
+    // If user email does not exist, create record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+      // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
+  });
+
+
+
+
 }
